4d2ce354c8b84efa39c802f48d2dc2c870e7a052
Petro Perehiniak <bronza.petro@gmail.com>
2022-02-26 01:29:56 +0200

    remove comments

--- serial_communication_trial.py
+++ serial_communication_trial.py
@@ -3,12 +3,12 @@ import serial
 import time
 ser = serial.Serial(baudrate=115200, timeout=0.5, port='COM3')
 time.sleep(2)
-print(ser)         # check which port was really used
+print(ser)
 command = b'start#delay#stop'
 print(command)
-ser.write(command)     # write a string
+ser.write(command)
 print("Check response")
 for _ in range(5):
     print(ser.readline())
     time.sleep(0.2)
-ser.close()             # close port
+ser.close()
